4db23862532da35f140ab61ac9d4b48906e2e89e
Mohan Barman <mohanbarman9711@gmail.com>
2023-10-25 23:18:25 +0530

    FIX: key expiring due to no ttl

--- commands/get.go
+++ commands/get.go
@@ -1,8 +1,8 @@
 package commands
 
 import (
-	"time"
 	"github.com/Mohanbarman/go-redis/resp"
+	"time"
 )
 
 func get(args []resp.Value, options Options) resp.Value {
@@ -20,10 +20,10 @@ func get(args []resp.Value, options Options) resp.Value {
 		return resp.Value{Typ: "null"}
 	}
 
-  if value.expiresAt.Sub(time.Now()) < 0 {
-    delete(SETs, key)
-    return resp.Value{Typ: "null"}
-  }
-  
+	if !value.expiresAt.IsZero() && value.expiresAt.Sub(time.Now()) < 0 {
+		delete(SETs, key)
+		return resp.Value{Typ: "null"}
+	}
+
 	return resp.Value{Typ: "bulk", Bulk: value.value}
 }
